4cef3e2b44de5ccf46ff4420b65b8e692867c73b
Nirina Razafimahafaly <nirina.razafimahafaly@santoshaconsulting.com>
2019-09-27 14:40:23 +0300

    change slide animation begin position to be from top or bottom

--- src/lib/src/animation-presets/slide.animation-preset.ts
+++ src/lib/src/animation-presets/slide.animation-preset.ts
@@ -18,28 +18,23 @@ export const slide: NotifierAnimationPreset = {
 			[ animatablePropertyName: string ]: string;
 		};
 
+		// Hide animation end position
+		let horizontalPosition: string;
+		if ( config.position.vertical.position === 'top' ) {
+			horizontalPosition = `calc( -100% - ${ config.position.horizontal.distance }px - 10px )`;
+		} else {
+			horizontalPosition = `calc( 100% + ${ config.position.horizontal.distance }px + 10px )`;
+		}
+
 		// Configure variables, depending on configuration and component
-		if ( config.position.horizontal.position === 'left' ) {
+		if ( config.position.horizontal.position === 'left' ||  config.position.horizontal.position === 'right' ) {
 			from = {
 				transform: `translate3d( 0, ${ shift }px, 0 )`
 			};
 			to = {
-				transform: `translate3d( calc( -100% - ${ config.position.horizontal.distance }px - 10px ), ${ shift }px, 0 )`
-			};
-		} else if ( config.position.horizontal.position === 'right' ) {
-			from = {
-				transform: `translate3d( 0, ${ shift }px, 0 )`
-			};
-			to = {
-				transform: `translate3d( calc( 100% + ${ config.position.horizontal.distance }px + 10px ), ${ shift }px, 0 )`
+				transform: `translate3d(0, ${ horizontalPosition }px, 0 )`
 			};
 		} else {
-			let horizontalPosition: string;
-			if ( config.position.vertical.position === 'top' ) {
-				horizontalPosition = `calc( -100% - ${ config.position.horizontal.distance }px - 10px )`;
-			} else {
-				horizontalPosition = `calc( 100% + ${ config.position.horizontal.distance }px + 10px )`;
-			}
 			from = {
 				transform: `translate3d( -50%, ${ shift }px, 0 )`
 			};
@@ -66,28 +61,23 @@ export const slide: NotifierAnimationPreset = {
 			[ animatablePropertyName: string ]: string;
 		};
 
+		// Always animate the container from the top or from the bottom 
+		let horizontalPosition: string;
+		if ( config.position.vertical.position === 'top' ) {
+			horizontalPosition = `calc( -100% - ${ config.position.horizontal.distance }px - 10px )`;
+		} else {
+			horizontalPosition = `calc( 100% + ${ config.position.horizontal.distance }px + 10px )`;
+		}
+
 		// Configure variables, depending on configuration and component
-		if ( config.position.horizontal.position === 'left' ) {
+		if ( config.position.horizontal.position === 'left' || config.position.horizontal.position === 'right' ) {
 			from = {
-				transform: `translate3d( calc( -100% - ${ config.position.horizontal.distance }px - 10px ), 0, 0 )`
-			};
-			to = {
-				transform: 'translate3d( 0, 0, 0 )'
-			};
-		} else if ( config.position.horizontal.position === 'right' ) {
-			from = {
-				transform: `translate3d( calc( 100% + ${ config.position.horizontal.distance }px + 10px ), 0, 0 )`
+				transform: `0, ${ horizontalPosition }, 0 )`
 			};
 			to = {
 				transform: 'translate3d( 0, 0, 0 )'
 			};
 		} else {
-			let horizontalPosition: string;
-			if ( config.position.vertical.position === 'top' ) {
-				horizontalPosition = `calc( -100% - ${ config.position.horizontal.distance }px - 10px )`;
-			} else {
-				horizontalPosition = `calc( 100% + ${ config.position.horizontal.distance }px + 10px )`;
-			}
 			from = {
 				transform: `translate3d( -50%, ${ horizontalPosition }, 0 )`
 			};
